352f1d0665179122a194e1b15a7c4c0d2ae31a51
PengLiang Wang <penwang2007@gmail.com>
2013-06-28 15:53:11 -0700

    Fix a bug: 0.0 is not equals -0.0

--- src/main/java/csplugins/jActiveModules/util/RankScaler.java
+++ src/main/java/csplugins/jActiveModules/util/RankScaler.java
@@ -1,47 +1,49 @@
 /*
-  File: RankScaler.java
+ File: RankScaler.java
 
-  Copyright (c) 2010, The Cytoscape Consortium (www.cytoscape.org)
+ Copyright (c) 2010, The Cytoscape Consortium (www.cytoscape.org)
 
-  This library is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation; either version 2.1 of the License, or
-  any later version.
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
 
-  This library is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
-  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
-  documentation provided hereunder is on an "as is" basis, and the
-  Institute for Systems Biology and the Whitehead Institute
-  have no obligations to provide maintenance, support,
-  updates, enhancements or modifications.  In no event shall the
-  Institute for Systems Biology and the Whitehead Institute
-  be liable to any party for direct, indirect, special,
-  incidental or consequential damages, including lost profits, arising
-  out of the use of this software and its documentation, even if the
-  Institute for Systems Biology and the Whitehead Institute
-  have been advised of the possibility of such damage.  See
-  the GNU Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications. In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage. See
+ the GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
-*/
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
 package csplugins.jActiveModules.util;
-
 
 import java.util.Arrays;
 import java.util.HashMap;
 
-
 /**
- *  Used to scale a list of values to [a,b]
+ * Used to scale a list of values to [a,b]
  */
 class RankScaler extends AbstractScaler {
-	public double[] scale(final double values[], final double a, final double b) throws IllegalArgumentException
-	{
+	private final long negativeZeroBitsLong = Double.doubleToRawLongBits(-0.0);
+	private final int negativeZeroBitsInt = Float.floatToRawIntBits(-0.0f);
+
+	public double[] scale(final double values[], final double a, final double b)
+			throws IllegalArgumentException {
 		if (values.length < 2)
-			throw new IllegalArgumentException("need at least 2 values for scaling!");
+			throw new IllegalArgumentException(
+					"need at least 2 values for scaling!");
 		if (a >= b)
 			throw new IllegalArgumentException("bad bounds!");
 
@@ -55,7 +57,9 @@ class RankScaler extends AbstractScaler {
 		double count = 1.0;
 		for (int i = 1; i < values.length; ++i) {
 			final double currentRankValue = stepSize * (0.5 + i);
-			if (sortedValues[i] == currentValue) {
+			if (sortedValues[i] == currentValue
+					&& ((Double.doubleToRawLongBits(sortedValues[i]) | Double
+							.doubleToRawLongBits(currentValue)) != negativeZeroBitsLong)) {
 				++count;
 				sum += currentRankValue;
 			} else {
@@ -65,7 +69,8 @@ class RankScaler extends AbstractScaler {
 				count = 1.0;
 			}
 		}
-		origValueToRankValueMap.put(sortedValues[values.length - 1], sum / count);
+		origValueToRankValueMap.put(sortedValues[values.length - 1], sum
+				/ count);
 
 		final double[] scaledValues = new double[values.length];
 		for (int i = 0; i < values.length; ++i)
@@ -74,10 +79,11 @@ class RankScaler extends AbstractScaler {
 		return scaledValues;
 	}
 
-	public float[] scale(final float values[], final float a, final float b) throws IllegalArgumentException
-	{
+	public float[] scale(final float values[], final float a, final float b)
+			throws IllegalArgumentException {
 		if (values.length < 2)
-			throw new IllegalArgumentException("need at least 2 values for scaling!");
+			throw new IllegalArgumentException(
+					"need at least 2 values for scaling!");
 		if (a >= b)
 			throw new IllegalArgumentException("bad bounds!");
 
@@ -91,7 +97,9 @@ class RankScaler extends AbstractScaler {
 		float count = 1.0f;
 		for (int i = 1; i < values.length; ++i) {
 			final float currentRankValue = stepSize * (0.5f + i);
-			if (sortedValues[i] == currentValue) {
+			if (sortedValues[i] == currentValue
+					&& ((Float.floatToRawIntBits(sortedValues[i]) | Float
+							.floatToRawIntBits(currentValue)) != negativeZeroBitsInt)) {
 				++count;
 				sum += currentRankValue;
 			} else {
@@ -101,7 +109,8 @@ class RankScaler extends AbstractScaler {
 				count = 1.0f;
 			}
 		}
-		origValueToRankValueMap.put(sortedValues[values.length - 1], sum / count);
+		origValueToRankValueMap.put(sortedValues[values.length - 1], sum
+				/ count);
 
 		final float[] scaledValues = new float[values.length];
 		for (int i = 0; i < values.length; ++i)
